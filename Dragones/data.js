cd741404295034b6b379039620f80c23761da7b3
camilaZalazar <128752097+camilaZalazar@users.noreply.github.com>
2023-11-03 20:12:58 -0300

    Add files via upload

---
+++ Dragones/data.js
@@ -0,0 +1,20 @@
+const Url = 'http://localhost:5500/Dragones/API.json';
+
+fetch(Url)
+  .then(response => response.json())
+  .then(data => {
+    const resultElement = document.getElementById('resultado');
+
+    data.forEach(api => {
+      const apiElement = document.createElement('div');
+      apiElement.innerHTML = `
+        <h2>${api.titulo}</h2>
+        <p>${api.parrafo}</p>
+        <img src="${api.img}" alt="${api.titulo} imagen">
+      `;
+      resultElement.appendChild(apiElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
