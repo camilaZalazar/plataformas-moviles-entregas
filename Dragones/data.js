4c9b8e7c84f2673b56388541e3db475eb19c59bd
camilaZalazar <128752097+camilaZalazar@users.noreply.github.com>
2023-11-24 20:01:37 -0300

    Update data.js

--- Dragones/data.js
+++ Dragones/data.js
@@ -9,7 +9,6 @@ fetch(Url)
       const apiElement = document.createElement('div');
       apiElement.innerHTML = `
         <h2 class="f">${api.titulo}</h2>
-        <div class="layout1">
         <p class="p">${api.parrafo}</p>
         <img src="${api.img}" alt="${api.titulo} height="500" width="500" imagen"></div>
         `;
