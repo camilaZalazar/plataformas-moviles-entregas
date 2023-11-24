c6188da8f7a3691673faf87cac2202d10a6556fa
camilaZalazar <128752097+camilaZalazar@users.noreply.github.com>
2023-11-24 19:32:22 -0300

    Update data.js

--- Dragones/data.js
+++ Dragones/data.js
@@ -3,15 +3,22 @@ const Url = 'https://camilazalazar.github.io/plataformas-moviles-entregas/Dragon
 fetch(Url)
   .then(response => response.json())
   .then(data => {
-    const resultElement = document.getElementById('resultado');
-
-    data.forEach(api => {
+    const resultElement = document.getElementById('resultado'); 
+    console.log(data)
+    data.dragones.forEach(api => {
       const apiElement = document.createElement('div');
       apiElement.innerHTML = `
-        <h2>${api.titulo}</h2>
-        <p>${api.parrafo}</p>
-        <img src="${api.img}" alt="${api.titulo} imagen">
-      `;
+        <h2 class="f">${api.titulo}</h2>
+        <div class="layout1">
+        <p class="p">${api.parrafo}</p>
+        <img src="${api.img}" alt="${api.titulo} height="500" width="500" imagen"></div>
+        `;
+        `
+        <div class="layout2">
+        <h2 class="f>${api.tit}<h2>
+        <p class="p">${api.pap}</p>
+        <img src="${api.imgag}" alt="${api.tit} height="700" width="700" imagen"></div>
+        `;
       resultElement.appendChild(apiElement);
     });
   })
